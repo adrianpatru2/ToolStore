ad5fa27b6aa1d4db52301f4de0f35c3caaea1529
Adrian Patru <adrian@Adrians-MacBook-Pro-2.local>
2024-06-04 17:42:51 -0400

    cleanup

--- src/main/java/com/toolstore/ToolStoreApplication.java
+++ src/main/java/com/toolstore/ToolStoreApplication.java
@@ -5,22 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Profile;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.client.Traverson;
 import org.springframework.web.client.RestTemplate;
 
-import com.toolstore.config.ToolStoreSecurity;
-import com.toolstore.config.ToolStoreUser;
-
 @SpringBootApplication
 public class ToolStoreApplication {
 	@Configuration
 	@Profile({ "pg", "h2" })
-	@ComponentScan(basePackages = { "com.toolstore", "com.toolstore.io" }, excludeFilters = {
-			@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = ToolStoreSecurity.class),
-			@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = ToolStoreUser.class) })
+	@ComponentScan(basePackages = { "com.toolstore" })
 	public class ConfigDb {
 	}
 
